e16d1193462b6f6fd5db95af1fdeea085dc29df2
Kazu26dr <pt.star3dr@gmail.com>
2025-04-24 15:17:50 +0900

    決済完了画面のスタイル修正

--- src/templates/OrderComplete.tsx
+++ src/templates/OrderComplete.tsx
@@ -24,16 +24,18 @@ const OrderComplete = () => {
   return (
     <div className="c-section-container">
       <h2 className="u-text__headline u-text-center">注文完了</h2>
-      <div className="c-container u-text-center">
+      <div className="c-container u-text-center u-text-left">
         <p>決済が完了しました。</p>
-        <p>注文番号は{orderId}です。</p>
+        <p>注文番号: <span className="u-text-bold">{orderId}</span></p>
         {stripePaymentId && (
-          <p>支払いID: {stripePaymentId}</p>
+          <p>支払いID: <span className="u-text-bold">{stripePaymentId}</span></p>
         )}
         <p>ご注文ありがとうございました。</p>
         <p>またのご利用をお待ちしております。</p>
         <div className="module-spacer--small" />
-        <PrimaryButton label="トップページへ戻る" onClick={handleClick} />
+        <div className="u-text-center">
+          <PrimaryButton label="トップページへ戻る" onClick={handleClick} />
+        </div>
       </div>
     </div>
   );
